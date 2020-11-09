d40766e08c78e95a752769b37776a8f31f553db2
Aseelsamer <assoul_08@hotmail.com>
2020-11-09 13:28:16 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -1,99 +1,126 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
 var category1= ['Mobile' , 'Laptop' , 'Tablet'];
 function fillCategory(){
   var fill = document.getElementById('Category');
   for (var i=0;i<category1.length;i++){
-    var options= document.createElement('p');
-    fill.appendChild(options);
+    var options= document.createElement('option');
     options.textContent=category1[i];
+    fill.appendChild(options);
   }
 }
 
 fillCategory();
 
 
-var devices=[];
-function Device(dName,category,quantity){
+// var devices=[];
+function Device(dName,category,quantity,unit){
   this.name=dName;
   this.category=category;
   this.quantity=quantity;
-  this.unit=0;
-  devices.all.push(this);
+  this.unit=unit;
+//   devices.all.push(this);
 }
 
-Device.prototype.price=function(){
-  this.unit=getRandomNumber();
-};
+// Device.prototype.price=function(){
+//   this.unit=getRandomNumber();
+// };
 
-function getRandomNumber(min,max){
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+// function getRandomNumber(min,max){
+//   return Math.floor(Math.random() * (max - min + 1) ) + min;
+// }
+
+var deviceList=JSON.parse(localStorage.getItem('deviceList'));
+if(deviceList){
+  Device.all=deviceList;
+  renderTable();
+}else{
+  Device.all=[];
 }
 
-
-function renderTableHeader(){
-  var header= document.getElementById('tableHeader');
-  var thE1=document.createElement('th');
-  header.appendChild(thE1);
-  thE1.textContent='Device Name';
-
-  var thE2=document.createElement('th');
-  header.appendChild(thE2);
-  thE2.textContent='Quantity';
-
-  var thE3=document.createElement('th');
-  header.appendChild(thE3);
-  thE3.textContent='Unit Price';
-
-  var thE4=document.createElement('th');
-  header.appendChild(thE4);
-  thE4.textContent='Category';
-
-}
-
-function renderTable(){
-  renderTableHeader();
-
-  var table=document.getElementById('tableSection');
-  for(var i=0;i<devices.all.length;i++){
-    var trE1=document.createElement('tr');
-    table.appendChild(trE1);
-
-    var td1=document.createElement('td');
-    trE1.appendChild(td1);
-    td1.textContent=this.dName;
-
-    var td2=document.createElement('td');
-    trE1.appendChild(td2);
-    td2.textContent=this.quantity;
-
-    var td3=document.createElement('td');
-    trE1.appendChild(td3);
-    td3.textContent=this.unit;
-
-    var td4=document.createElement('td');
-    trE1.appendChild(td4);
-    td4.textContent=this.category;
-
-  }
-}
 
 
 
 var myForm=document.getElementById('myForm');
-myForm.addEventListener('submit',onClickRender());
+myForm.addEventListener('submit',onClickRender);
 
 function onClickRender(e){
-//   e.preventDefault();
-
-  renderTable();
+  e.preventDefault();
+//   renderTableHeader();
+  //   renderTable();
   var name= e.target.ItemName.value;
   var category=e.target.Category.value;
   var quantity= e.target.Quantity.value;
-  var unit= getRandomNumber();}
+  var unit= Math.floor(Math.random()* 7) +1 ;
+  var deviceItem= new Device(name,category,quantity,unit);
+  localStorage.setItem('deviceList',JSON.stringify(Device.all));
+  var deviceTable = document.getElementById('tableSection');
+  deviceTable.textContent='';
+  renderTable();
+  var myForm=document.getElementById('myForm');
 
-// if (e.target.id )
+  myForm.reset();
 
-// }
+}
+function renderTable(){
+  renderTableHeader();
 
-Device.unit(350,750);
+  var table=document.getElementById('tableSection');
+  var tableBody=document.createElement('tbody');
+  for(var i=0;i<Device.all.length;i++){
+    var trE1=document.createElement('tr');
+    // table.appendChild(trE1);
+
+    var td1=document.createElement('td');
+    trE1.appendChild(td1);
+    td1.textContent=Device.all[i].dName;
+
+    var td2=document.createElement('td');
+    trE1.appendChild(td2);
+    td2.textContent=Device.all[i].quantity;
+
+    var td3=document.createElement('td');
+    trE1.appendChild(td3);
+    td3.textContent=Device.all[i].unit;
+
+    var td4=document.createElement('td');
+    trE1.appendChild(td4);
+    td4.textContent=Device.all[i].category;
+
+    tableBody.appendChild(trE1);
+
+  }
+  table.appendChild(tableBody);
+}
+
+function renderTableHeader(){
+  var header= document.getElementById('tableSection');
+  var thE1=document.createElement('th');
+  var tableRoww=document.createElement('tr');
+
+  var th1=document.createElement('th');
+  tableRoww.appendChild(th1);
+  th1.textContent='Device Name';
+
+  var thE2=document.createElement('th');
+  tableRoww.appendChild(thE2);
+  thE2.textContent='Quantity';
+
+  var thE3=document.createElement('th');
+  tableRoww.appendChild(thE3);
+  thE3.textContent='Unit Price';
+
+  var thE4=document.createElement('th');
+  tableRoww.appendChild(thE4);
+  thE4.textContent='Category';
+
+  thE1.appendChild(tableRoww);
+  header.appendChild(thE1);
+
+}
+
+
+
+
+
